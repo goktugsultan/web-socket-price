afcae7dacc7cdb1c2302245f75ea41a712ef3eac
Goktug SULTAN <goktugsultan@gmail.com>
2020-02-12 01:19:24 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { WebSocketPrice } from './WebSocketPrice'
+
+function App() {
+  return (
+    <div className="App">
+      <WebSocketPrice />
+    </div>
+  );
+}
+
+export default App;
